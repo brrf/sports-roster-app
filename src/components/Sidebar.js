baac7b1e6a7831d16ee3c4e29674cc8477044b5c
Moshe Praver <moshepraver@gmail.com>
2019-03-03 14:43:14 -0600

    fetch and render player data

---
+++ src/components/Sidebar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Proptypes from 'prop-types'
+import {Link, Route} from 'react-router-dom'
+import slug from 'slug'
+
+Sidebar.propTypes = {
+	title: Proptypes.string.isRequired,
+	list: Proptypes.array.isRequired,
+	loading: Proptypes.bool.isRequired
+}
+
+function CustomLink ({to, children}) {
+	return (
+		<Route
+			path={to.pathname}
+			children={({ match }) => (
+				<li style={{listStyleType: 'none', fontweight: match ? 'bold' : 'none'}}>
+					<Link to={to}>{children}</Link>
+				</li>
+			)} />
+	)
+}
+
+export default function Sidebar ({title, list, loading, location, match}) {
+		if (loading === true) {
+			return <div>Loading...</div>
+		}
+		return (
+			<div>
+				<h3 className='header'>{title}</h3>
+				<ul className='sidebar-list'>
+					{list.map( (item) => (
+						<CustomLink
+							key={item}
+							to={{
+								pathname: `${match.url}/${slug(item)}`,
+								search: location.search
+							}}>
+							{item.toUpperCase()}
+						</CustomLink>
+					))}
+				</ul>
+			</div>
+		)
+}
